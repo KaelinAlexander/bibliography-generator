9aadfa1a7fbcd0be6997fac2324904e6ce847671
KaelinAlexander <kaelin.alexander@gmail.com>
2020-08-18 13:52:58 -0600

    session debugging & first validations & error displays

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,22 @@ class UsersController < ApplicationController
     end
 
     def new
+        @user = User.new
     end
 
     def create
         if params[:user][:password] == params[:user][:password_confirmation]
-            @user = User.create(user_params)
-            session[:user_id] = @user.id
-            session[:username] = @user.username
-            redirect_to @user
+            @user = User.new(user_params)
+                if @user.valid?
+                    @user.save
+                    session[:user_id] = @user.id
+                    session[:username] = @user.username
+                    redirect_to @user
+                else
+                    render :new
+                end
         else
-            redirect_to signup_path
+           render :new
         end
     end
     
