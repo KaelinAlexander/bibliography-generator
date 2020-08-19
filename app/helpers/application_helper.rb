bd8e7b6a07ae58b0da193920d22eedad1e19cb92
KaelinAlexander <kaelin.alexander@gmail.com>
2020-08-19 12:20:27 -0600

    generates title, author, editor successfully

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,6 +40,101 @@ module ApplicationHelper
         end 
     end
 
+    def get_authors(text_hash)
+        authors = []
+        if text_hash["name"]
+            if text_hash["name"].class == Hash
+                if text_hash["name"]["namePart"].class == String
+                    authors << text_hash["name"]["namePart"]
+                else
+                    authors << text_hash["name"]["namePart"][0]
+                end
+            else
+                text_hash["name"].each do |author|
+                    if author["namePart"].class == String
+                        authors << author["namePart"] unless author["@type"] != "personal"
+                    else
+                        authors << author["namePart"][0] unless author["@type"] != "personal"
+                    end
+                end
+            end
+        else
+            authors << "No author."
+        end
+        if authors == []
+            authors << "No author."
+        end
+        authors
+    end
+
+    def edited?(text_hash)
+        check_hash = {}
+        if text_hash["note"] && text_hash["note"].class != String
+            text_hash["note"].each do |h|
+                if h.class == Hash
+                    h.each do |k, v|
+                        check_hash[k] = v if v.include?("edited")
+                    end
+                else
+                    false
+                end
+            end
+            if check_hash.empty?
+                false
+            else
+                true
+            end
+        else
+            false
+        end
+    end
+
+    def get_editors(text_hash)
+        # edited?(text_hash)
+        #     check_hash
+        #     byebug
+        # else 
+        #     nil
+        # end
+
+        # if text_hash["note"]
+          check_hash = ""
+            text_hash["note"].each do |h|
+                if h.class == Hash
+                    h.each do |k, v|
+                        if v.include?("edited")
+                            check_hash = v
+                        else
+                            nil
+                        end
+                    end
+                end
+            end
+        check_hash.gsub("edited by", "").chop
+    end
+
+    def author_cleanup(author_array)
+        if author_array != ["No author."]
+            string_authors = ""
+            no_period = []
+            author_array.each do |author|
+                if author[-1] == "."
+                    author = author.chop
+                    a = author.split(", ")
+                else
+                    a = author.split(", ")
+                end
+                a[0], a[1] = a[1], a[0]
+                full_name = a.join(" ")
+                string_authors << "#{full_name}, "
+            end
+            chopped = string_authors.chop.chop
+        else
+            chopped = "No author."
+        end
+    end
+
+
     def logged_in
         !!session[:user_id]
     end
